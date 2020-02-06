7c7587c33958ac55bf2e72571f21372f7654c544
wykeenjenga <46722362+wykeenjenga@users.noreply.github.com>
2020-02-06 18:55:21 +0300

    i have made some changes

--- app/src/main/java/com/example/cashin/Entrance.java
+++ app/src/main/java/com/example/cashin/Entrance.java
@@ -1,6 +1,8 @@
 package com.example.cashin;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -8,44 +10,37 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.cashin.ui.login.LoginActivity;
 
 
-public class Entrance extends Fragment {
-    private Context activity;
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_entrance, container, false);
-
-    }
+public class Entrance extends Activity {
+    Button login,register;
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_entrance);
 
-        Button btn = getView().findViewById(R.id.btnLogin);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ((ButtonCommand) activity).BtnClicked("Login");
-            }
-        });
     }
+    public void openForms(View view){
+        if (view.getId()==R.id.btnLogin){
+            Intent intent=new Intent(this,LoginActivity.class);
+            startActivity(intent);
+        }
+        else if (view.getId()==R.id.btnRegEntr){
+            Intent intent=new Intent(this,RegisterActivity.class);
+            startActivity(intent);
+        }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        activity = context;
-    }
-
-    public interface ButtonCommand{
-        public void BtnClicked(String btn);
     }
 }
--- app/src/main/java/com/example/cashin/MainActivity.java
+++ app/src/main/java/com/example/cashin/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -16,52 +17,50 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements introPage.onFragmentComplete,
-        Login.onfragComplete, Entrance.ButtonCommand {
-    FragmentManager fragmentManager;
-    FragmentTransaction fragmentTransaction;
+import java.util.Timer;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class MainActivity extends AppCompatActivity{
+
+    TextView textView;
+    CircleImageView circleImageView;
+
+    ///my splash screen>>..................
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        introPage fragment = new introPage();
-        fragmentTransaction.add(R.id.rootLayout, fragment);
-        fragmentTransaction.commit();
-    }
 
-    public void fragComplete(String frag){
-        //show fragment entrance
-        if (frag.equals("introPage")){
-            Entrance fragment = new Entrance();
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.rootLayout, fragment);
-            fragmentTransaction.commit();
-        }
-        else if(frag.equals("Menu"))
-        {
-            Menu fragment = new Menu();
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.rootLayout, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-        }
-    }
 
-    public void BtnClicked(String btn){
-        //show fragment login
-        if (btn=="Login"){
-            Login fragment = new Login();
-            fragmentTransaction = fragmentManager.beginTransaction();
-            fragmentTransaction.replace(R.id.rootLayout, fragment);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-        }
+        textView=(TextView) findViewById(R.id.TV);
+        circleImageView=(CircleImageView) findViewById(R.id.IV);
+
+        Animation animation=AnimationUtils.loadAnimation(this,R.anim.mytransation);
+
+        final Intent i=new Intent(this,Entrance.class);
+
+        textView.startAnimation(animation);
+        circleImageView.startAnimation(animation);
+
+        Thread timer=new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(i);
+                    finish();
+                }
+
+            }
+        };
+        timer.start();
+
 
     }
-
-
 }
